69979d6c54bdf919e1552f916621b6fd5e5b7263
Jari-Pekka Teurajärvi <teurajarvi@hotmail.com>
2017-08-27 23:48:32 +0300

    Update setyp.py

--- setyp.py
+++ setyp.py
@@ -1,10 +1,10 @@
 from distutils.core import setup
 
-setup(name='Website_monitor',
+setup(name='website_monitor',
       version='1.0',
-      description='Website monitor',
+      description='Website monitor tool',
       author='Jari-Pekka Teurajarvi',
       author_email='teurajarvi@hotmail.com',
       url='https://github.com/teurajarvi/website_monitor/',
-      packages=['website_monitor'],
+      packages=['.controllers','.models']
      )
