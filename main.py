299efc3828e1d87c1ca65b1074c290a42870f36b
Jari-Pekka Teurajärvi <teurajarvi@hotmail.com>
2017-08-27 23:00:02 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 # main.py
+'''
+This is the main of the mebsite_monitor
+'''
 import sys
 sys.path.insert(0, sys.path[0]+'\\controllers')
 
@@ -7,7 +10,7 @@ import logging
 from monitor import monitor_pages
 
 logging.basicConfig(filename='monitor.log', level=logging.INFO)
-logging.info('main started')
+logging.info('main of the system_monitor started')
 monitor_pages()
-logging.info('main finished')
+logging.info('main of the system_monitor finished')
 
