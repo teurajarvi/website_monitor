88d553b9152323cb9883c439fc7bd04396fcaf4e
Jari-Pekka Teurajärvi <teurajarvi@hotmail.com>
2017-08-28 04:15:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,10 +7,11 @@ import sys
 sys.path.insert(0, sys.path[0]+'\\')
 
 import logging
+import logs.log
 from controllers.monitor import monitor_pages
 
-logging.basicConfig(filename='monitor.log', level=logging.INFO)
 logging.info('main of the system_monitor started')
+# start monitoring the web pages
 monitor_pages()
 logging.info('main of the system_monitor finished')
 
