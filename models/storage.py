e4594ea55b23ba16b067b2266f0ae07aac04028b
Jari-Pekka Teurajärvi <teurajarvi@hotmail.com>
2017-08-28 04:30:42 +0300

    Update storage.py

--- models/storage.py
+++ models/storage.py
@@ -4,7 +4,7 @@
 def get_headers():
     '''
     Read User-Agent header value
-	'''
+    '''
     print("start get_headers")
     headers = {'User-Agent': 'Mozilla/5.0 (iPad; U; CPU OS 3_2_1 like Mac OS X; en-us) AppleWebKit/531.21.10 (KHTML, like Gecko) Mobile/7B405'}
     print("finish get_headers")
@@ -13,7 +13,7 @@ def get_headers():
 def get_urls():
     '''
     Read URLs by validation text from appconfig.ini
-	'''
+    '''
     import configparser
     config = configparser.ConfigParser()
     config.read("./private/appconfig.ini")
