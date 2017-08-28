87178987fd4d856d477bdff89b25cf4f7c6b37c6
Jari-Pekka Teurajärvi <teurajarvi@hotmail.com>
2017-08-28 04:20:07 +0300

    Update setyp.py

--- setyp.py
+++ setyp.py
@@ -1,16 +1,19 @@
 from distutils.core import setup
 
 # Install website_monitor with commands:
-# >cd <root forder of the website_monitor>
+# >cd [root forder of the website_monitor]
 # >python setyp.py install
-# run the application whit command:
+# run the application with command:
 # >main.py
+# Check the config.ini file to change the application values
+# Check the monitor_log.log file to track user logs
 
 setup(name='website_monitor',
       version='1.0',
-      description='Website monitor tool',
+      description='application to monitor website response time and simple text content validation',
       author='Jari-Pekka Teurajarvi',
       author_email='teurajarvi@hotmail.com',
       url='https://github.com/teurajarvi/website_monitor/',
-      packages=['.controllers','.models']
+      packages=['.controllers', '.models', '.logs']
      )
+
