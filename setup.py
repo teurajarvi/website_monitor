cdfb5aee6d758583f85c794ae924892834ccd8c7
Jari-Pekka Teurajärvi <teurajarvi@hotmail.com>
2017-08-28 10:01:27 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,7 @@
-from distutils.core import setup
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 
 # Install website_monitor with commands:
 # >cd [folder website_monitor-master]
@@ -11,21 +14,22 @@ from distutils.core import setup
 # Check the config.ini file to change the application values
 # Check the monitor_log.log file to track user logs
 
-setup(name='website_monitor',
-      version='1.0',
-      description='application to monitor website response time and simple text content validation',
-      author='Jari-Pekka Teurajarvi',
-      author_email='teurajarvi@hotmail.com',
-      url='https://github.com/teurajarvi/website_monitor/',
-      license='MIT',
-      classifiers=[
-        #   4 - Beta
-        #   5 - Production/Stable
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'Topic :: Software Development :: Build Tools',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 2.7'],
-      packages=['.controllers', '.models', '.logs'],
-      install_requires=['requests','beautifulsoup4','lxml','configparser']
+setup(
+    name='website_monitor',
+    version='1.0',
+    description='application to monitor website response time and simple text content validation',
+    author='Jari-Pekka Teurajarvi',
+    author_email='teurajarvi@hotmail.com',
+    url='https://github.com/teurajarvi/website_monitor/',
+    license='MIT',
+    classifiers=[
+      #   4 - Beta
+      #   5 - Production/Stable
+      'Development Status :: 4 - Beta',
+      'Intended Audience :: Developers',
+      'Topic :: Software Development :: Build Tools',
+      'License :: OSI Approved :: MIT License',
+      'Programming Language :: Python :: 2.7'],
+    packages=['.controllers', '.models', '.logs'],
+    install_requires=['requests','beautifulsoup4','lxml','configparser']
 )
